c88aeb29c92798dbb8c4cbdc07cc8eb3fd8de483
Nitis Chimpayak <nitischimpayak@gmail.com>
2026-04-12 21:33:36 +0700

    update status controller

--- booking-stadium-backend/controllers/statsController.js
+++ booking-stadium-backend/controllers/statsController.js
@@ -1,10 +1,10 @@
-import User from "../models/User.js";
-import Booking from "../models/Booking.js";
+import User from "../models/Userr.js";
+import Booking from "../models/Bookingg.js";
 
 // ✅ API: จำนวนคน Register ทั้งหมด
 export const getTotalRegisteredUsers = async (req, res) => {
     try {
-        const userCount = await User.countDocuments(); // นับจำนวนผู้ใช้ทั้งหมด
+        const userCount = await User.count();; // นับจำนวนผู้ใช้ทั้งหมด
         res.status(200).json({ totalUsers: userCount });
     } catch (error) {
         res.status(500).json({ message: "Failed to fetch user count", error });
@@ -14,7 +14,11 @@ export const getTotalRegisteredUsers = async (req, res) => {
 // ✅ API: จำนวนยอดจองที่ยังไม่ยืนยัน
 export const getPendingBookings = async (req, res) => {
     try {
-        const pendingCount = await Booking.countDocuments({ status: "pending" }); // นับจำนวนการจองที่ status = pending
+        const pendingCount = await Booking.count({
+            where: {
+                status: "pending"
+            }
+        }); // นับจำนวนการจองที่ status = pending
         res.status(200).json({ pendingBookings: pendingCount });
     } catch (error) {
         res.status(500).json({ message: "Failed to fetch pending bookings count", error });
